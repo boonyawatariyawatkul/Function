1dba1841fdc9b45aed2469b5d08f5440f4a19b16
boonyawatariyawatkul <ariyawatkulb@gmail.com>
2020-04-08 14:18:11 +0700

    Initial Commit

---
+++ Function.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+def subtractionNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+addNumber(3,4)
+subtractionNumber(8,3)
+multiplyNumber(5,4)
+divideNumber(4,9)
